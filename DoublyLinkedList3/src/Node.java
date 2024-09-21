662f2f715c493d78519d3d18e98490d0a719674b
Süleyman Güzey <suleymanguzey590@gmail.com>
2024-09-21 20:29:54 +0300

    Implement a Doubly Linked List with Enqueue and Dequeue Operations
    - Added a DoublyLinkedList class that supports adding nodes to the end of the list (enqueue) and removing nodes from the front of the list (dequeue).
    - Implemented a Node class to represent individual elements in the list, maintaining references to both the next and previous nodes.

---
+++ DoublyLinkedList3/src/Node.java
@@ -0,0 +1,12 @@
+public class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    // Node constructor
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
